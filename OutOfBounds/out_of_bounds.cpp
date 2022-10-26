b01c5c630ae5491dbf1fe9d99e7c8b624860fb1c
Shoaib Meenai <smeenai@fb.com>
2022-10-26 10:56:58 -0700

    Out of bounds

---
+++ OutOfBounds/out_of_bounds.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+  std::vector<int> numbers = { 0, 1, 2 };
+  std::cout << "numbers[0] is " << numbers[0] << "\n";
+  std::cout << "numbers[1] is " << numbers[1] << "\n";
+  std::cout << "numbers[2] is " << numbers[2] << "\n";
+  std::cout << "numbers[3] is " << numbers[3] << "\n";
+}
