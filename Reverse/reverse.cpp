09893bf22ebc2afc4b760a0ab8f7d3b7eb2bcbcb
Shoaib Meenai <smeenai@fb.com>
2022-10-26 11:45:17 -0700

    Reverse

---
+++ Reverse/reverse.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+std::string reverseString(std::string str) {
+  return str;
+}
+
+int main() {
+  std::string str = "hello world";
+  std::cout << str << " backwards is " << reverseString(str) << "\n";
+}
